400c735ed54c09ba81dd9e892d02c98b00351562
thaibs080603-tech <thaibs080603@gmail.com>
2025-12-03 12:36:58 +0700

    Add: Vercel Insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import { Toaster } from "@/components/ui/toaster"
 
@@ -21,6 +22,7 @@ export default function RootLayout({
             <body className={inter.className}>
                 {children}
                 <Toaster />
+                 <SpeedInsights />
             </body>
         </html>
     )
